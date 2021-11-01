0df29f04d5bc95550188d7c9b8374ff4bb7a3248
leedonghu <ldh930517@gmail.com>
2021-11-02 00:45:46 +0900

    baekjoon

--- src/baekjoon/Q2775.java
+++ src/baekjoon/Q2775.java
@@ -7,20 +7,30 @@ import java.io.InputStreamReader;
 public class Q2775 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		
 		for(int i=0; i<n; i++) {
 			int h = Integer.parseInt(br.readLine());
 			int w = Integer.parseInt(br.readLine());
-			int[] arr = new int[w];
 			
-
-			for(int j=1; j<h; j++) {
-				int plus = 1;
-				for(int k=0; k<w; k++) {
-					
+			int[][] arr = new int[15][15]; //h, w의 범위가 1~14
+			for(int j=0; j<15; j++) { //1층을 1 2 3 4 5... n층의 0번을 1로 초기화
+				arr[j][1] = 1;
+				arr[0][j] = j;
+			}
+			
+			for(int j=1; j<15; j++) { //1층에서 14층
+				for(int k=2; k<15; k++) {//2호에서 14호
+					arr[j][k] = arr[j-1][k] + arr[j][k-1];
 				}
 			}
+
+			sb.append(arr[h][w]).append("\n"); //3층의 2호는 2층의 2호 + 3층의 1호
+
+
 		}
+		
+		System.out.println(sb.toString());
 	}
 }
---
+++ src/baekjoon/Q2839.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2839 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int kg = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int a1 = kg % 5;
+		int a2 = kg / 5;
+		
+		if(a1 % 3 == 0) {
+			
+		}
+	}
+}
