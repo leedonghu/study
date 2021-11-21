5af0b58b4f261db6a50e50384fe319d55c58153d
leedonghu <ldh930517@gmail.com>
2021-11-22 01:10:37 +0900

    algorithm

--- src/algorithm/recursiveAlgorithm/EuclidGCD.java
+++ src/algorithm/recursiveAlgorithm/EuclidGCD.java
@@ -27,32 +27,37 @@ public class EuclidGCD {
 		return max;
 	}
 	
-	static int gcdArray(int[] a) {
-		int max = a[0];
-		for(int i=0; i<a.length; i++) {
-			if(max < a[i]) {
-				max = a[i];
-			}
-		}
+	static int gcdArray(int[] a) { //A B 의 공약수 D, A B C의 최대공약수는 D 와 C의 최대공약수와 같음
+		int n = 0;
+		int gcd = 0;
 		
-		boolean check = true;
-		for(int i=2; i<max; i++) {
-//			for()
+		while(n<a.length-1) {
+			gcd = gcd(a[n], a[n+1]);
+			n++;
 		}
-		
-		return (check == true) ? max : 1;
+		return gcd;
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println("두 정수의 최대공약수를 구합니다.");
+//		System.out.println("두 정수의 최대공약수를 구합니다.");
+//		
+//		System.out.print("정수를 입력하세요 : ");
+//		int x = Integer.parseInt(br.readLine());
+//		System.out.print("정수를 입력하세요 : ");
+//		int y = Integer.parseInt(br.readLine());
+//		
+//		System.out.println("최대공약수는 " + gcd02(x,y) + "입니다.");
 		
-		System.out.print("정수를 입력하세요 : ");
-		int x = Integer.parseInt(br.readLine());
-		System.out.print("정수를 입력하세요 : ");
-		int y = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int t = 0;
+		while(t<n) {
+			arr[t] = Integer.parseInt(br.readLine());
+			t++;
+		}
 		
-		System.out.println("최대공약수는 " + gcd02(x,y) + "입니다.");
+		System.out.println("최대공약수는 " + gcdArray(arr) + "입니다.");
 	}
 }
