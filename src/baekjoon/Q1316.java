939729f2fa63fbfce6c88ba8117242d913be3a90
leedonghu <ldh930517@gmail.com>
2021-12-27 22:41:36 +0900

    baekjoon

--- src/baekjoon/Q1316.java
+++ src/baekjoon/Q1316.java
@@ -9,15 +9,46 @@ public class Q1316 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int n = Integer.parseInt(br.readLine());
-		for(int i=0; i<n; i++) {
+		int test = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		
+		for(int i=0; i<test; i++) {
 			String str = br.readLine();
-			
-			for(int j=0; i<str.length(); j++) {
-				if(str.charAt(j) == str.charAt(j+1)) {
-					
-				}
+			if(check(str) == true) {
+				cnt++;
 			}
 		}
+		
+		System.out.println(cnt);
+		
+	}
+	
+	public static boolean check(String str) {
+		//문자열이 중복되었는지 확인하는 배열
+		boolean[] check = new boolean[26];
+		
+		//이전의 문자를 저장하는 변수
+		int prev = 0;
+		
+		for(int i=0; i<str.length(); i++) {
+			
+			//이전 문자와 현재의 문자가 같지 않을때
+			if(prev != str.charAt(i)) {
+				//지금의 문자가 이전에 나왔던 적이 없다면
+				if(check[str.charAt(i) - 'a'] == false) {
+					//해당 문자가 위치하는 배열을 true로 바꿔줌
+					check[str.charAt(i) - 'a'] = true;
+					//이전 문자를 현재문자로 저장
+					prev = str.charAt(i);
+				}else {
+					//지금의 문자가 이미 나왔었다면 false를 return
+					return false;
+				}
+				
+			}
+			
+		}
+		//for문을 다 돌고도 false를 return 하지 않는다면 true를 리턴
+		return true;
 	}
 }
