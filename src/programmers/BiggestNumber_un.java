8fba4438e9afb77ee78d4a5cc5a0c9a917c79d40
leedonghu <ldh930517@gmail.com>
2022-07-21 22:18:45 +0900

    programmers

--- src/programmers/BiggestNumber_un.java
+++ src/programmers/BiggestNumber_un.java
@@ -1,43 +1,35 @@
 package programmers;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class BiggestNumber_un {
 	public static void main(String[] args) {
 		int[] numbers = {3, 30, 34, 5, 9,7,10,74};
-		solution(numbers);
+		System.out.println(solution(numbers));
 	}
 	
 	static String solution(int[] numbers) {
 		String[] arr = new String[numbers.length];
-//		String a = "3";
-//		String b = "30";
-//		System.out.println(a.compareTo(b));
-//		if(a.compareTo(b) > 0) {
-//			System.out.println(a);
-//		}else {
-//			System.out.println(b);
-//		}
 		for(int i=0; i<numbers.length; i++) {
 			arr[i] = String.valueOf(numbers[i]);
 		}
 		
-		for(int i=0; i<4; i++) {
-			for(int j=0; j<numbers.length-1; j++) {
-				for(int k=numbers.length-1; k>j; k--) {
-					System.out.println(i);
-					if(arr[k-1].length()>=i+1 && arr[k].length() >= i+1) {
-						if(arr[k-1].charAt(i) < arr[k].charAt(i)) {
-							String tmp = arr[k-1];
-							arr[k-1] = arr[k];
-							arr[k] = tmp;						}
-					}
-				}
+		Arrays.sort(arr, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				
+				return (o2+o1).compareTo(o1+o2);
 			}
+		});
+		if(arr[0] == "0") {
+			return "0";
 		}
-		
+		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<arr.length; i++) {
-			System.out.println("arr : " + arr[i]);
+			
+			sb.append(arr[i]);
 		}
-		
-		return null;
+		return sb.toString();
 	}
 }
---
+++ src/programmers/OpenChattingRoom.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class OpenChattingRoom {
+	static String[] solution(String[] record) {
+		
+		return null;
+	}
+	
+	class User{
+		String inOut;
+		String id;
+		String nickName;
+	}
+}
