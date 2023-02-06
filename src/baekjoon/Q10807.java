b89fd9f67d38d3d6da9584269a2cf0409e3c68c4
leedonghu <ldh930517@gmail.com>
2023-02-06 21:38:41 +0900

    2/6 beakjoon

---
+++ src/baekjoon/Q10807.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10807 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+
+        int k = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == v){
+                k++;
+            }
+        }
+        
+
+        System.out.println(k);
+    }
+}
