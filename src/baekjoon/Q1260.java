495c0a02d40d787dc84a33f0ac2aba8c7c919500
leedonghu <ldh930517@gmail.com>
2023-02-09 23:21:32 +0900

    2/9

--- src/baekjoon/Q1260.java
+++ src/baekjoon/Q1260.java
@@ -5,66 +5,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Q1260 {
-	static int[][] check; //간선 연결 상태
-	static boolean[] checked; //확인 여부
-	static int n; //정점갯수
-	static int m; //간선갯수
-	static int start; //시작정점
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int[][] map;
+	static boolean[] check;
+	static StringBuilder sb;
+	static int n;
+	static int m;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		start = Integer.parseInt(br.readLine());
-		
-		check = new int[1001][1001]; //좌표를 그대로 받기 위해 +1함
-		checked = new boolean[1001];
-		
-		//간선 연결상태 저장
-		for(int i=0; i<m; i++) {
-			int x = Integer.parseInt(br.readLine());
-			int y = Integer.parseInt(br.readLine());
-			
-			check[x][y] = check[y][x] = 1;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
+
+		map = new int[n+1][n+1];
+		check = new boolean[n+1];
+		sb = new StringBuilder();
+		for(int i=0; i<m; i++){
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			map[a][b] = map[b][a] = 1;
 		}
-		
-		dfs(start); //dfs호출
-		
-		checked = new boolean[1001]; //확인상태 초기화
-		System.out.println(); //줄바꿈
-		
-		dfs(); //dfs호출
+
+		dfs(start);
+		check = new boolean[n+1];
+		sb.append("\n");
+		bfs(start);
+		System.out.println(sb.toString());
 	}
-	
-	//시작점을 변수로 받아 확인, 출력 후 다음 연결점을 찾아 시작점을 변경하여 재호출
-	public static void dfs(int i) {
-		checked[i] = true;
-		System.out.print(i + " ");
-		
-		for(int j=1; j<=n; j++) {
-			if(check[i][j] == 1 && checked[j] == false) {
-				dfs(j);
+
+	static void dfs(int start){
+		check[start] = true;
+		sb.append(start).append(" ");
+		for(int i=1; i<=n; i++){
+			if(map[start][i] == 1 && check[i] == false){
+				dfs(i);
 			}
 		}
 	}
-	
-	public static void dfs() {
-		Queue<Integer>queue = new LinkedList<Integer>();
-		queue.offer(start); //시작점 넣음
-		checked[start] = true;
-		System.out.print(start + " ");
-		
-		//queue가 빌 때까지 반복. 방문 정점은 확인, 출력 후 Queue에 넣어 순서대로 확인
-		while(!queue.isEmpty()) {
-			int temp = queue.poll();
-			
-			for(int j=1; j<=n; j++) {
-				if(check[temp][j] == 1 && checked[j] == false) {
-					queue.offer(j);
-					checked[j] = true;
-					System.out.print(j + " ");
+
+	static void bfs(int start){
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(start);
+		check[start] = true;
+		sb.append(start).append(" ");
+		while(!q.isEmpty()){
+			int t = q.poll();
+
+			for(int i=1; i<=n; i++){
+				if(map[t][i] == 1 && check[i] == false){
+					
+					q.offer(i);
+					check[i] = true;
+					sb.append(i).append(" ");
 				}
 			}
 		}
